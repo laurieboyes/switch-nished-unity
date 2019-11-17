1054159c1f31067127906b33aa0575b643b5bb3f
Laurie Boyes <laurie.boyes@ovoenergy.com>
2019-11-17 19:16:25 +0000

    Camera movement with keyboard

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private float speed = 7;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
