2f854d03868215e5597ee2064c9562ae8dc73711
Laurie Boyes <laurie.boyes@ovoenergy.com>
2019-11-17 10:49:05 +0000

    Create 10 houses on a row in a grid

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Grid grid;
+    private List<GameObject> structures;
+
+    void Start()
+    {
+        Object dwellingPrefab = Resources.Load("Dwelling");
+        structures = new List<GameObject>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject dwelling = Instantiate(dwellingPrefab) as GameObject;
+            Structure dwellingStructure = dwelling.GetComponent<Structure>();
+            dwellingStructure.gridPosition = new Vector2Int(i, 0);
+
+            dwelling.transform.position = grid.CellToWorld(new Vector3Int(dwellingStructure.gridPosition.x, dwellingStructure.gridPosition.y, 0));
+
+            structures.Add(dwelling);
+
+        }
+    }
+
+    void Update()
+    {
+    }
+}
